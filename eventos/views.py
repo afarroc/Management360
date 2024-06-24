578c99c1f6babea87d23a84df39e93a6d7204feb
Arturo <arturofarro1@gmail.com>
2024-06-24 13:58:16 -0500

    Changue event status error depuration

--- eventos/views.py
+++ eventos/views.py
@@ -558,8 +558,6 @@ class ProfileView(View):
         except Exception as e:
             return render(request, 'profiles/error.html', {'message': str(e)})
 
-
-
 class ViewProfileView(View):
     def get(self, request, user_id):
         profile = Profile.objects.get(user_id=user_id)
