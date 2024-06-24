606b615157ae1032baa715200d50f8e8701ec782
Arturo <arturofarro1@gmail.com>
2024-06-23 19:55:51 -0500

    //# sourceMappingURL=skin.js.map

--- py.py
+++ py.py
@@ -1,14 +1,15 @@
 import os
 
-def buscar_texto(ruta, texto):
+def eliminar_lineas(ruta, texto):
     for raiz, dirs, archivos in os.walk(ruta):
         for archivo in archivos:
             ruta_archivo = os.path.join(raiz, archivo)
             with open(ruta_archivo, 'r') as f:
                 lineas = f.readlines()
-                for num_linea, linea in enumerate(lineas, start=1):
-                    if texto in linea:
-                        print(f'Archivo: {ruta_archivo}, Línea: {num_linea}, Texto: {linea.strip()}')
+            with open(ruta_archivo, 'w') as f:
+                for linea in lineas:
+                    if texto not in linea:
+                        f.write(linea)
 
 # Reemplaza '/ruta/a/tu/proyecto' con la ruta real a tu directorio de proyecto
-buscar_texto('/static', 'js.map')
+eliminar_lineas('/pyvirtual/', '//# sourceMappingURL=skin.js.map')
