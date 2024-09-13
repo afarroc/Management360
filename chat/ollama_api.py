9c86171c2fb8995e3976906dc0fc9528a0616ee7
afarroc <108610976+afarroc@users.noreply.github.com>
2024-09-13 04:50:54 -0500

    Assistant config changes

--- chat/ollama_api.py
+++ chat/ollama_api.py
@@ -3,28 +3,18 @@ from ollama import AsyncClient
 client = AsyncClient(host='192.168.18.40:11434')
 
 async def generate_response(prompt):
-    stream = await client.chat(
-        model='gemma2:2b', 
-        messages=[
-            {
-                'role': 'user',
-                'content': "Eres un asistente de Analista de datos y responderás solo en español."
-                },
-            {
-                "role": "assistant",
-                "content": "De acuerdo, soy un asistente de Analista de datos y responderé solo en español"
-                },
-            {
-                "role": "user",
-                "content": prompt
-                }
-                ],
-        stream=True,
-        options= {
-            "temperature": 0.1,
-          },
+	stream = await client.chat(
+		model='gemma2:2b', 
+		messages=[
 
-    )
-    async for chunk in stream:
-        print(chunk)
-        yield chunk
+			{"role":"user",	"content": prompt}
+				],
+		stream=True,
+		options= {
+			"temperature": 0.7,
+		  },
+
+	)
+	async for chunk in stream:
+		print(chunk)
+		yield chunk
