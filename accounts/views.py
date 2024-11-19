d99c7416e1fea61158e5cfece4b70eddf536f904
Arturo <arturofarro1@gmail.com>
2024-11-19 05:52:25 -0500

    Account view

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,9 @@
-# accounts/views.py
-
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 
 from .forms import UserLoginForm, UserRegistrationForm
-
-# Create your views here.
 
 def indexView(request):
     return render(request,'index.html')
@@ -17,6 +13,7 @@ def dashboardView(request):
     return render(request,'dashboard.html')
 
 def register_view(request):
+    page_title="Register"
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
@@ -25,29 +22,26 @@ def register_view(request):
             return redirect('home')
     else:
         form = UserRegistrationForm()
-    return render(request, 'register.html', {'form': form})
-
-# accounts/views.py
-from django.shortcuts import render, redirect
-from .forms import UserLoginForm
-from django.contrib.auth import authenticate, login
-
-from django.shortcuts import render, redirect
-from .forms import UserLoginForm
-from django.contrib.auth import authenticate, login
+    return render(request, 'register.html', {'form': form, 'page_title':page_title})
 
 def login_view(request):
-  if request.method == 'POST':
-    form = UserLoginForm(request.POST)
-    if form.is_valid():  
-      username = form.cleaned_data['username']
-      password = form.cleaned_data['password']
-      user = authenticate(username=username, password=password) or authenticate(email=username, password=password)
-      if user is not None:
-        login(request, user)
-        if 'remember' in form.cleaned_data and form.cleaned_data['remember']:
-          request.session.set_expiry(604800) 
-        return redirect('home')
-  else:
-    form = UserLoginForm()
-  return render(request, 'login.html', {'form': form, 'title': 'Login'})
+    page_title="Login"
+
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():  
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password) or authenticate(email=username, password=password)
+            if user is not None:
+                login(request, user)
+                if 'remember' in form.cleaned_data and form.cleaned_data['remember']:
+                    request.session.set_expiry(604800) 
+                return redirect('home')
+    else:
+        form = UserLoginForm()
+    return render(request, 'login.html', {'form': form, 'page_title': page_title})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
