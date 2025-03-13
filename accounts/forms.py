62e9872ad0d6c38ce8d86e4462c53919ba34ac38
Arturo <arturofarro1@gmail.com>
2025-03-13 02:18:21 -0500

    Accounts fix basic functionallity

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,18 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class SignUpForm(UserCreationForm):
-    email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
+    email = forms.EmailField(max_length=254, required=True, 
+                           help_text='Required. Enter a valid email address.')
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("This email address is already in use.")
+        return email
