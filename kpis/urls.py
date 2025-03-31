cd6234034e66972512e462fcb128c89cf3c74138
afarroc <108610976+afarroc@users.noreply.github.com>
2025-03-31 05:21:41 -0500

    Add data generation form and template; update URLs and include form field template

--- kpis/urls.py
+++ kpis/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('dashboard/', views.aht_dashboard, name='dashboard'),  
     # Exportar datos para Power BI/Tableau  
     path('export-data/', views.export_data, name='export_data'),  
+    
+    path('generate-data/', views.generate_fake_data, name='generate_data'),
+
 ]  
