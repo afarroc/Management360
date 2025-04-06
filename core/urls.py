14bdd5868b1986e11304d9ac7ba21cffe43c1053
Arturo <arturofarro1@gmail.com>
2025-04-06 06:32:37 -0500

    Add new templates for various pages and update navigation structure
    - Created new 'about.html' template with basic layout and example cards.
    - Added 'about_old_panda.html' template with animated panda images and upload functionality.
    - Introduced 'blank.html' template for a blank page layout.
    - Developed 'contact.html' template with contact form and information boxes.
    - Implemented 'faq.html' template with accordion-style FAQ sections.
    - Established 'home.html' template with dashboard layout and user-specific content.
    - Updated navigation includes for account management, configuration, curriculum, dashboard, events, KPIs, management, pages, projects, rooms, and tasks.

--- core/urls.py
+++ core/urls.py
@@ -10,9 +10,7 @@ urlpatterns = [
     path('setup/', SetupView.as_view(), name='setup'),
     path('<int:days>/', views.home_view, name="index"),
     path('<int:days>/<int:days_ago>/', views.home_view, name="index"),
-    path('about/', views.about, name="about"),
     path('faq/', views.faq, name="faq"),
-    path('contact/', views.contact, name="contact"),
     path('blank/', views.blank, name="blank"),
     ]
 
