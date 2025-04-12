5cbf4c29735a8ce53cc37093a3e36bc7f2eb683c
afarroc <108610976+afarroc@users.noreply.github.com>
2025-04-12 14:45:34 -0500

    Refactor accounts and tools apps: remove file_tree_view from accounts, add file_tree_view to tools, and update URL patterns accordingly

--- tools/urls.py
+++ tools/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import file_tree_view
 
 urlpatterns = [
     path('calculate_agents/', views.calculate_agents, name='calculate_agents'),
+    # File tree view
+    path("file-tree/", file_tree_view, name="file_tree"),
 ]
