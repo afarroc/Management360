21631d96b3773fb77d82f60c024d6827c09f0b57
afarroc <108610976+afarroc@users.noreply.github.com>
2025-06-02 17:20:07 -0500

    Add KPI Home page and update navigation links

--- kpis/urls.py
+++ kpis/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views  
 
 urlpatterns = [  
+    path('', views.kpi_home, name='kpi_home'),
     # Dashboard de AHT  
     path('dashboard/', views.aht_dashboard, name='dashboard'),  
     # Exportar datos para Power BI/Tableau  
     path('export-data/', views.export_data, name='export_data'),      
     path('generate-data/', views.generate_fake_data, name='generate_data'),
-]  
+]
